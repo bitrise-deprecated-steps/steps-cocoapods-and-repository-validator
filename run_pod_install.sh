d894b4926e868cdc8ba170665f0c11530a479cbc
Viktor <viktor>
2014-05-16 02:47:38 +0200

    run_pod_install now supports multiple Podfiles and Podfiles which are not in the repository's root directory

--- run_pod_install.sh
+++ run_pod_install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-if [ -f "Podfile" ]; then
-  echo "$ pod install"
-  pod install
-fi
+podcount=0
+IFS=$'\n'
+for podfile in $(find . -type f -name 'Podfile')
+do
+  podcount=$[podcount + 1]
+  echo " (i) Podfile found at: $podfile"
+  echo " (i) Podfile directory: $(dirname \"$podfile\")"
+  echo "$ (cd $(dirname \"$podfile\") && pod install)"
+  (cd $(dirname "$podfile") && pod install)
+done
+unset IFS
+echo " (i) Found Podfile count: $podcount"
