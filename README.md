12c30c96821ee0b6fcbe6164527e0d8b51196825
erosdome <erosdome@gmail.com>
2014-08-13 11:55:29 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 steps-cocapods-and-repository-validator
 =======================================
 
-# Environment
-- BITRISE_SOURCE_DIR
-- GATHER_PROJECTS
-- BITRISE_STEP_DIR
+# Input Environment Variables
+- **BITRISE_SOURCE_DIR**
+
+	the source folder of the application
+- **GATHER_PROJECTS**
+
+	optional, if set, step scans for project files and project configurations in the BITRISE_SOURCE_DIR
+- **BITRISE_STEP_DIR**
+
+	the directory of this step
--- step.sh
+++ step.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function echo_string_to_formatted_output {
+  echo "$1" >> $formatted_output_file_path
+}
+
+function write_section_to_formatted_output {
+  echo '' >> $formatted_output_file_path
+  echo "$1" >> $formatted_output_file_path
+  echo '' >> $formatted_output_file_path
+}
+
 echo "$ cd $BITRISE_SOURCE_DIR"
 cd $BITRISE_SOURCE_DIR
 if [ $? -ne 0 ]; then
@@ -28,4 +38,9 @@ if [ -n "$GATHER_PROJECTS" ]; then
   done
 else
   $BITRISE_STEP_DIR/run_pod_install.sh
+  if [ $? -ne 0 ]; then
+    echo " [!] Pod install failed!"
+    write_section_to_formatted_output "Pod install failed!"
+    exit 1
+  fi
 fi
