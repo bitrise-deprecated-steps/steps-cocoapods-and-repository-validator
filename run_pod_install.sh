bd56615a56163ffaf55954d0d56c85f66f00c30b
Viktor Benei <viktor.benei@gmail.com>
2014-11-15 20:40:54 +0100

    private cocoapods pod spec repo fix for SSH podspec repos

--- run_pod_install.sh
+++ run_pod_install.sh
@@ -4,6 +4,7 @@ THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${THIS_SCRIPTDIR}/_utils.sh"
 source "${THIS_SCRIPTDIR}/_formatted_output.sh"
 
+CONFIG_cocoapods_ssh_source_fix_script_path="${THIS_SCRIPTDIR}/cocoapods_ssh_source_fix.rb"
 
 write_section_to_formatted_output "### Searching for podfiles and installing the found ones"
 
@@ -16,8 +17,8 @@ do
   curr_podfile_dir=$(dirname "${podfile}")
   echo " (i) Podfile directory: ${curr_podfile_dir}"
 
-  (cd "${curr_podfile_dir}" && pod install)
-  if [ $? -ne 0 ]; then
+  (cd "${curr_podfile_dir}" && ruby "${CONFIG_cocoapods_ssh_source_fix_script_path}" --podfile="${podfile}" && pod install)
+  if [ $? -ne 0 ] ; then
     write_section_to_formatted_output "Could not install podfile: ${podfile}"
     exit 1
   fi
