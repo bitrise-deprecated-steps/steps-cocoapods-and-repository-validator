86205252e1a64cc883a4ae349a27ca0f572e44d9
Viktor Benei <viktor.benei@gmail.com>
2014-11-15 21:14:47 +0100

    podfile path fix

--- run_pod_install.sh
+++ run_pod_install.sh
@@ -15,9 +15,10 @@ do
   podcount=$[podcount + 1]
   echo " (i) Podfile found at: ${podfile}"
   curr_podfile_dir=$(dirname "${podfile}")
+  curr_podfile_basename=$(basename "${podfile}")
   echo " (i) Podfile directory: ${curr_podfile_dir}"
 
-  (cd "${curr_podfile_dir}" && ruby "${CONFIG_cocoapods_ssh_source_fix_script_path}" --podfile="${podfile}" && pod install)
+  (cd "${curr_podfile_dir}" && ruby "${CONFIG_cocoapods_ssh_source_fix_script_path}" --podfile="${curr_podfile_basename}" && pod install)
   if [ $? -ne 0 ] ; then
     write_section_to_formatted_output "Could not install podfile: ${podfile}"
     exit 1
