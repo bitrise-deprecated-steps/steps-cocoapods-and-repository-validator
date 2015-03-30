34d5eef958239b59942e9e1ec8dd0524740fc930
Viktor Benei <viktor.benei@gmail.com>
2015-03-30 13:34:10 +0200

    modifications to use the new bash_utils & pod install is now more verbose

--- run_pod_install.sh
+++ run_pod_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "${THIS_SCRIPTDIR}/_utils.sh"
-source "${THIS_SCRIPTDIR}/_formatted_output.sh"
+source "${THIS_SCRIPTDIR}/_bash_utils/utils.sh"
+source "${THIS_SCRIPTDIR}/_bash_utils/formatted_output.sh"
 
 CONFIG_cocoapods_ssh_source_fix_script_path="${THIS_SCRIPTDIR}/cocoapods_ssh_source_fix.rb"
 
@@ -13,12 +13,19 @@ IFS=$'\n'
 for podfile in $(find . -type f -iname 'Podfile')
 do
   podcount=$[podcount + 1]
-  echo " (i) Podfile found at: ${podfile}"
+  echo " ====> Podfile found at: ${podfile}"
   curr_podfile_dir=$(dirname "${podfile}")
   curr_podfile_basename=$(basename "${podfile}")
   echo " (i) Podfile directory: ${curr_podfile_dir}"
 
-  (cd "${curr_podfile_dir}" && ruby "${CONFIG_cocoapods_ssh_source_fix_script_path}" --podfile="${curr_podfile_basename}" && pod install --verbose)
+  (
+    cd "${curr_podfile_dir}"
+    fail_if_cmd_error "Failed to cd into dir: ${curr_podfile_dir}"
+    ruby "${CONFIG_cocoapods_ssh_source_fix_script_path}" --podfile="${curr_podfile_basename}"
+    fail_if_cmd_error "Failed to fix Podfile: ${curr_podfile_basename}"
+    pod install --verbose
+    fail_if_cmd_error "Failed to pod install"
+  )
   if [ $? -ne 0 ] ; then
     write_section_to_formatted_output "Could not install podfile: ${podfile}"
     exit 1
