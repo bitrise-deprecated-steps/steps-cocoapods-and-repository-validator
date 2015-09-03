271bb830fb5db2eb5874bd3290b84d835352e73f
Viktor Benei <viktor.benei@gmail.com>
2015-09-03 22:29:43 +0200

    major revision: this step is now designed for collecting project infos, no "only pod install" mode is left.
    It's now separated into the steps-cocoapods-install step, which is included here with `depman`
    `_setup.sh` was also removed, it's now handled as a `step.yml` dependency

--- step.sh
+++ step.sh
@@ -3,7 +3,6 @@
 THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${THIS_SCRIPTDIR}/_bash_utils/utils.sh"
 source "${THIS_SCRIPTDIR}/_bash_utils/formatted_output.sh"
-source "${THIS_SCRIPTDIR}/_setup.sh"
 
 # init / cleanup the formatted output
 echo "" > "${formatted_output_file_path}"
@@ -15,59 +14,66 @@ if [ -z "${source_root_path}" ]; then
   exit 1
 fi
 
-# Update Cocoapods
-if [[ "${is_update_cocoapods}" != "false" ]] ; then
-  print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/steps-cocoapods-update/step.sh"
+# Update Cocoapods - if there's at least one Podfile
+podfile_find_out="$(find . -iname 'Podfile')"
+is_podfile_found=0
+if [[ "${podfile_find_out}" != "" ]] ; then
+  is_podfile_found=1
 else
-  write_section_to_formatted_output "*Skipping Cocoapods version update*"
+  write_section_to_formatted_output "*No Podfile found*"
+fi
+
+if [ ${is_podfile_found} -eq 1 ] ; then
+  if [[ "${is_update_cocoapods}" != "false" ]] ; then
+    print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/_steps-cocoapods-update/step.sh"
+  else
+    write_section_to_formatted_output "*Skipping Cocoapods version update*"
+  fi
+else
+  write_section_to_formatted_output "*Skipping CocoaPods update (No Podfile found)*"
 fi
 
 print_and_do_command_exit_on_error cd "${source_root_path}"
 
-if [[ "${gather_project_infos}" == "yes" ]]; then
-  write_section_to_formatted_output "# Gathering project configurations"
-  # create/cleanup ~/.schemes file
-  echo "" > ~/.schemes
 
-  if [ ! -z "${scan_only_branch}" ] ; then
-    write_section_to_formatted_output "*Scanning a single branch: ${scan_only_branch}*"
-    branches_to_scan=("origin/${scan_only_branch}")
-  else
-    write_section_to_formatted_output "*Scanning all branches*"
-    branches_to_scan=$(git branch -r | grep -v -- "->")
-  fi
-  echo " (i) branches_to_scan:"
-  echo "${branches_to_scan}"
-  set -e
-  echo "===> Setting up gather-projects dependencies..."
-  SetupGatherProjectsDependencies
-  echo "<=== Dependency setup completed."
-  set +e
-  for branch in ${branches_to_scan} ; do
-    echo
-    echo "==> Switching to branch: ${branch}"
-    # remove every file before switch; except the .git folder
-    print_and_do_command_exit_on_error find . -not -path '*.git/*' -not -path '*.git' -delete
-    # remove the prefix "origin/" from the branch name
-    branch_without_remote=$(printf "%s" "${branch}" | cut -c 8-)
-    echo "Local branch: ${branch_without_remote}"
-    # switch to branch
-    GIT_ASKPASS=echo GIT_SSH="${THIS_SCRIPTDIR}/ssh_no_prompt.sh" git checkout -f "${branch_without_remote}"
-    fail_if_cmd_error "Failed to checkout branch: ${branch_without_remote}"
-    GIT_ASKPASS=echo GIT_SSH="${THIS_SCRIPTDIR}/ssh_no_prompt.sh" git submodule foreach git reset --hard
-    fail_if_cmd_error "Failed to reset submodules"
-    GIT_ASKPASS=echo GIT_SSH="${THIS_SCRIPTDIR}/ssh_no_prompt.sh" git submodule update --init --recursive
-    fail_if_cmd_error "Failed to update submodules"
+write_section_to_formatted_output "# Gathering project configurations"
+# create/cleanup ~/.schemes file
+echo "" > ~/.schemes
 
-    write_section_to_formatted_output "### Switching to branch: ${branch_without_remote}"
-
-    print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/run_pod_install.sh"
-    print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/find_schemes.sh" "${branch_without_remote}"
-    echo "-> Finished on branch: ${branch}"
-  done
+if [ ! -z "${scan_only_branch}" ] ; then
+  write_section_to_formatted_output "*Scanning a single branch: ${scan_only_branch}*"
+  branches_to_scan=("origin/${scan_only_branch}")
 else
-  write_section_to_formatted_output "# Run pod install"
-  print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/run_pod_install.sh"
+  write_section_to_formatted_output "*Scanning all branches*"
+  branches_to_scan=$(git branch -r | grep -v -- "->")
 fi
+echo " (i) branches_to_scan:"
+echo "${branches_to_scan}"
+for branch in ${branches_to_scan} ; do
+  echo
+  echo "==> Switching to branch: ${branch}"
+  # remove every file before switch; except the .git folder
+  print_and_do_command_exit_on_error find . -not -path '*.git/*' -not -path '*.git' -delete
+  # remove the prefix "origin/" from the branch name
+  branch_without_remote=$(printf "%s" "${branch}" | cut -c 8-)
+  echo "Local branch: ${branch_without_remote}"
+  # switch to branch
+  GIT_ASKPASS=echo GIT_SSH="${THIS_SCRIPTDIR}/ssh_no_prompt.sh" git checkout -f "${branch_without_remote}"
+  fail_if_cmd_error "Failed to checkout branch: ${branch_without_remote}"
+  GIT_ASKPASS=echo GIT_SSH="${THIS_SCRIPTDIR}/ssh_no_prompt.sh" git submodule foreach git reset --hard
+  fail_if_cmd_error "Failed to reset submodules"
+  GIT_ASKPASS=echo GIT_SSH="${THIS_SCRIPTDIR}/ssh_no_prompt.sh" git submodule update --init --recursive
+  fail_if_cmd_error "Failed to update submodules"
+
+  write_section_to_formatted_output "### Switching to branch: ${branch_without_remote}"
+
+  if [ ${is_podfile_found} -eq 1 ] ; then
+    export is_update_cocoapods="false" # if required it's already handled
+    print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/_steps-cocoapods-install/run_pod_install.sh"
+  fi
+  print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/find_schemes.sh" "${branch_without_remote}"
+  echo "-> Finished on branch: ${branch}"
+done
+
 
 exit 0
