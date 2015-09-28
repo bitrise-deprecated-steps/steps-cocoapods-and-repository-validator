7892a6dc3cb0acf8bf810b4012889324b7ae91cd
Viktor Benei <viktor.benei@gmail.com>
2015-09-28 16:38:42 +0200

    depman-update : updated pod-install

--- _steps-cocoapods-install/step.sh
+++ _steps-cocoapods-install/step.sh
@@ -4,15 +4,13 @@ THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${THIS_SCRIPTDIR}/_bash_utils/utils.sh"
 source "${THIS_SCRIPTDIR}/_bash_utils/formatted_output.sh"
 
-# init / cleanup the formatted output
-echo "" > "${formatted_output_file_path}"
-
 
 if [ -z "${source_root_path}" ]; then
   write_section_to_formatted_output "# Error"
   write_section_start_to_formatted_output '* source_root_path input is missing'
   exit 1
 fi
+print_and_do_command_exit_on_error cd "${source_root_path}"
 
 # Update Cocoapods
 if [[ "${is_update_cocoapods}" != "false" ]] ; then
@@ -28,8 +26,6 @@ else
 
   write_section_start_to_formatted_output "CocoaPods version: ${pod_version}"
 fi
-
-print_and_do_command_exit_on_error cd "${source_root_path}"
 
 write_section_to_formatted_output "# Run pod install"
 print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/run_pod_install.sh"
