4c604ecf4ad64c7569eff8e7973fe3b269d864e1
Krisztian Godrei <godrekrisztian@gmil.com>
2015-10-12 18:52:09 +0100

    utils moved to main

--- handle_xamarin_ios.go
+++ handle_xamarin_ios.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/base64"
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,6 +12,7 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strings"
 )
 
@@ -19,6 +21,66 @@ const (
 	unifiedXamarinIosAPIName = "Xamarin.iOS"
 	monotouchAPIName         = "monotouch"
 )
+
+// ---------------
+// Utils
+
+// WriteStringToFile ...
+func WriteStringToFile(pth string, fileCont string) error {
+	return WriteBytesToFile(pth, []byte(fileCont))
+}
+
+// WriteBytesToFileWithPermission ...
+func WriteBytesToFileWithPermission(pth string, fileCont []byte, perm os.FileMode) error {
+	if pth == "" {
+		return errors.New("No path provided")
+	}
+
+	var file *os.File
+	var err error
+	if perm == 0 {
+		file, err = os.Create(pth)
+	} else {
+		// same as os.Create, but with a specified permission
+		//  the flags are copy-pasted from the official
+		//  os.Create func: https://golang.org/src/os/file.go?s=7327:7366#L244
+		file, err = os.OpenFile(pth, os.O_RDWR|os.O_CREATE|os.O_TRUNC, perm)
+	}
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println(" [!] Failed to close file:", err)
+		}
+	}()
+
+	if _, err := file.Write(fileCont); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// WriteBytesToFile ...
+func WriteBytesToFile(pth string, fileCont []byte) error {
+	return WriteBytesToFileWithPermission(pth, fileCont, 0)
+}
+
+// UserHomeDir ...
+func UserHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
+}
+
+// ---------------
+// Models
 
 // SchemeModel ...
 type SchemeModel struct {
@@ -69,25 +131,8 @@ type ProjectModel struct {
 	ItemGroups []ItemGroup `xml:"ItemGroup"`
 }
 
-func (project ProjectModel) String() string {
-	ret := "Project:\n"
-	ret += " PropertyGroups:\n"
-	if len(project.ItemGroups) > 0 {
-		ret += " ItemGroups:\n"
-		for _, itemGroup := range project.ItemGroups {
-			if len(itemGroup.Reference) > 0 {
-				ret += "  ItemGroup:\n"
-				for _, reference := range itemGroup.Reference {
-					if reference.Include != "" {
-						ret += "    Include: " + reference.Include + "\n"
-					}
-				}
-				ret += "\n"
-			}
-		}
-	}
-	return ret
-}
+// ---------------
+// Step
 
 func getSolutionFiles() ([]string, error) {
 	solutions := []string{}
