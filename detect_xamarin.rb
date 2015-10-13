db8011328cf708820fb443ae22eb2dbbeb17640f
Krisztian Godrei <godrekrisztian@gmil.com>
2015-10-13 15:27:52 +0100

    fixes

--- detect_xamarin.rb
+++ detect_xamarin.rb
@@ -6,10 +6,10 @@ require 'pathname'
 # -----------------------
 
 def xamarin_ios_project(project_file_path)
-  query = 'Xamarin.iOS'
+  query = '<Reference Include="Xamarin.iOS" />'
   unified_ios_api = File.readlines(project_file_path).grep(/#{query}/).size > 0
 
-  query = 'monotouch'
+  query = '<Reference Include="monotouch" />'
   ios_api = File.readlines(project_file_path).grep(/#{query}/).size > 0
 
   (unified_ios_api || ios_api)
@@ -19,13 +19,10 @@ end
 # --- main
 # -----------------------
 
-project_root_dir = Dir.pwd
-solution_file_paths = Find.find(project_root_dir).select { |p| /.*\.sln$/ =~ p }
-
 ios_project_file_paths = []
-(solution_file_paths).each do |solution_file_path|
-  puts "-----------------------------------"
-  puts "solution_file_path: #{solution_file_path}"
+Dir["**/*.sln"].each do |solution_file_path|
+  puts
+  puts "-> solution_file_path: #{solution_file_path}"
   puts
 
   pn = Pathname.new(solution_file_path)
@@ -38,27 +35,18 @@ ios_project_file_paths = []
     project_file_name = splits[1].gsub(/\\/, '/').delete! '""'
     project_file_path = File.join(solution_base_dir_path, project_file_name)
 
-    puts
-    puts "project_file_path: #{project_file_path}"
-    puts
+    puts "  -> project_file_path: #{project_file_path}"
 
     is_xamarin_ios = xamarin_ios_project(project_file_path)
 
-    puts
-    puts "is_xamarin_ios: #{is_xamarin_ios}"
+    puts "  -> is_xamarin_ios: #{is_xamarin_ios}"
     puts
 
     if is_xamarin_ios
       ios_project_file_paths.push(project_file_path)
     end
 
-    puts "----------------"
-
   end
-
-  puts
-  puts "-----------------------------------"
-  puts
 end
 
 puts
