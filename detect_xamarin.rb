922611743e4987830671bd36e08536fe72095d92
Krisztian Godrei <godrekrisztian@gmil.com>
2015-10-13 15:18:54 +0100

    log fixes

--- detect_xamarin.rb
+++ detect_xamarin.rb
@@ -21,10 +21,13 @@ end
 
 project_root_dir = Dir.pwd
 solution_file_paths = Find.find(project_root_dir).select { |p| /.*\.sln$/ =~ p }
-puts "solution_file_paths: #{solution_file_paths}"
 
 ios_project_file_paths = []
 (solution_file_paths).each do |solution_file_path|
+  puts "-----------------------------------"
+  puts "solution_file_path: #{solution_file_path}"
+  puts
+
   pn = Pathname.new(solution_file_path)
   solution_base_dir_path, solution_file_name = pn.split
 
@@ -36,23 +39,30 @@ ios_project_file_paths = []
     project_file_path = File.join(solution_base_dir_path, project_file_name)
 
     puts
-    puts "raw_path: #{project_file_path}"
+    puts "project_file_path: #{project_file_path}"
     puts
 
-    if xamarin_ios_project(project_file_path)
+    is_xamarin_ios = xamarin_ios_project(project_file_path)
+
+    puts
+    puts "is_xamarin_ios: #{is_xamarin_ios}"
+    puts
+
+    if is_xamarin_ios
       ios_project_file_paths.push(project_file_path)
     end
+
+    puts "----------------"
+
   end
+
+  puts
+  puts "-----------------------------------"
+  puts
 end
 
 puts
 puts "xamarin ios projects: #{ios_project_file_paths}"
-puts
-
-exit_code = ios_project_file_paths.count > 0
-
-puts
-puts "exit_code: #{exit_code}"
 puts
 
 exit (ios_project_file_paths.count > 0 ? 0 : 1)
